37d3fb4c468e081fe9d4d7b3512f139c159db558
elhombre <elhombre.dev@gmail.com>
2019-12-05 12:01:34 +0500

    Setup workflow

---
+++ README.md
@@ -0,0 +1,5 @@
+# Aperos RPC common
+
+Useful TypeScript classes suitable for both browser and Node.js.
+The development branch can contain experimental code that is under
+active development and should not be used for production environments.
---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import path from 'path'
+import pluginTypescript from 'rollup-plugin-typescript2'
+import { terser as pluginTerser } from 'rollup-plugin-terser'
+import pkg from './package.json'
+
+const external = [
+  ...Object.keys(process.binding('natives')),
+  ...Object.keys(pkg.dependencies || {})
+]
+
+const targetDir = 'dist'
+
+const plugins = [pluginTypescript({
+  useTsconfigDeclarationDir: true,
+  verbosity: 1 })]
+
+if (process.env.BUILD === 'production') {
+  plugins.push(
+    pluginTerser({
+      output: { comments: false },
+      toplevel: true
+    })
+  )
+}
+
+export default [
+  {
+    input: 'lib/index.ts',
+    external,
+    output: {
+      file: path.join(targetDir, 'index.js'),
+      format: 'cjs',
+      name: 'index',
+      sourcemap: true
+    },
+    plugins
+  }
+]
