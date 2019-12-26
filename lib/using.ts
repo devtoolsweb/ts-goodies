cac65b1f7b10ff1de4fbd81610f479c7996dd527
elhombre <elhombre.dev@gmail.com>
2019-12-26 18:36:49 +0500

    Allow calling "using ()" both asynchronously and synchronously

--- lib/using.ts
+++ lib/using.ts
@@ -1,10 +1,10 @@
 interface IDisposable {
-  dispose(): Promise<void>
+  dispose(): Promise<void> | void
 }
 
 export async function using<T extends IDisposable>(
   resource: T,
-  body: (resource: T) => Promise<void>
+  body: (resource: T) => Promise<void> | void
 ) {
   try {
     await body(resource)
