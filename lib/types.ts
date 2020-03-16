a9f375763772d8550828a58fd0aa0132f0da6990
elhombre <elhombre.dev@gmail.com>
2020-03-16 19:50:51 +0500

    Add generic type FixedLengthArray

--- lib/types.ts
+++ lib/types.ts
@@ -3,3 +3,31 @@ export interface IConstructor<T = {}> {
 }
 
 export type Constructor<T = {}> = new (...args: any[]) => T
+
+// export type FixedLengthArray<T, L extends number, TObj = [T, ...Array<T>]> = Pick<
+//   TObj,
+// Exclude<keyof TObj, ArrayLengthMutationKeys>
+// > & {
+//   readonly length: L
+//   [I: number]: T
+//   [Symbol.iterator]: () => IterableIterator<T>
+// }
+
+export type ArrayLengthMutationKeys = 'splice' | 'push' | 'pop' | 'shift' | 'unshift' | number
+
+export type ArrayItems<T extends Array<any>> = T extends Array<infer TItems> ? TItems : never
+
+export type FixedLengthArray<T extends any[]> = Pick<
+  T,
+  Exclude<keyof T, ArrayLengthMutationKeys>
+> & {
+  [Symbol.iterator]: () => IterableIterator<ArrayItems<T>>
+}
+
+export type IndexedFixedLengthArray<T extends any[]> = Pick<
+  T,
+  Exclude<keyof T, ArrayLengthMutationKeys>
+> & {
+  [I: number]: T extends Array<infer TItems> ? TItems : never
+  [Symbol.iterator]: () => IterableIterator<ArrayItems<T>>
+}
